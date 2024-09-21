bb4538cb16efb817e9ee6d0044d7c9fd154ad9b0
danieltwentynine <danielmcardoso2016@protonmail.com>
2024-09-21 10:37:23 -0300

    adding about modal

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,3 +5,31 @@ const themeToggleButton = document.getElementById('toggleLightTheme');
 themeToggleButton.addEventListener('click', () => {
     document.body.classList.toggle('lightTheme');
 });
+
+//--------------------------------------------------------------//
+
+// Get the modal
+const modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+const aboutLink = document.getElementById("about");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("modal__content_close")[0];
+
+// When the user clicks the button, open the modal
+aboutLink.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
